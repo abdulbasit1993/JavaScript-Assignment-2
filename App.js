7860a51a4dacf4d1024d5dd58f96c0e375b7c22e
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2020-06-21 21:47:11 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+// Chapters 21 - 25
+
+// Task # 1
+var firstName = prompt("Enter your First name:");
+var lastName = prompt("Enter your Last name:");
+var fullName = firstName + " " + lastName;
+document.write("Welcome " + fullName + " !");
+
+// Task # 2
+document.write("<br><br>");
+var favMobile = prompt("Enter your favorite mobile phone model:");
+document.write("My favorite phone is: " + favMobile);
+document.write("<br>");
+document.write("Length of string: " + favMobile.length);
